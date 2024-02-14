0d34072e653f3987936664fec3e9d035b090144c
Dominic Burfeind <153544939+Dazzy1709@users.noreply.github.com>
2024-02-14 12:13:01 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+const addButton = document.getElementById("addToCart_button");
+const shoppingCart = document.getElementById("idCart");
+const productQuantity = document.getElementById("product_amount");
+const checkOutButton = document.getElementById("checkout_button");
+const trashCan = document.getElementById("trashCan");
+
+
+
+
+// Eventlisteners
+
+
+
+
+
+
+function addToCart() {
+
+  };
+
+function loadPage() {
+
+  };
+
+function loadCategory() {
+
+  };
+
+function removeFromCart() {
+
+  };
+
+function cartCosts() {
+
+  };
+
+function productPricing() {
+
+  };
